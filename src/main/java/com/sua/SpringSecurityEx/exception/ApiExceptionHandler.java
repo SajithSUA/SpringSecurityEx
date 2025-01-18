64d2d7fd0be94d88c391d7ee8451b6f8b15a63bd
SajithSUA <001sajith@gmail.com>
2025-01-19 01:36:34 +0530

    Add global exception handler to rest controller classes

---
+++ src/main/java/com/sua/SpringSecurityEx/exception/ApiExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.sua.SpringSecurityEx.exception;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * This class is used to handle exceptions in the all rest controller class.
+ */
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    /**
+     * This method is used to handle ObjectNotFoundException in all rest controller class.
+     * In this we use Spring AOP concept to handle global exception.
+     */
+    @ExceptionHandler(value = {ObjectNotFoundException.class})
+    public ResponseEntity<String> handleApiRequestException(ObjectNotFoundException e) {
+        return new ResponseEntity<String>(e.getEntityName(), HttpStatus.NOT_FOUND);
+    }
+}
